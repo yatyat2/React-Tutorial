c4cd18489790c23c5a6fca5b2420263175acd8c2
yatyat2 <xowls2287@naver.com>
2018-08-02 18:50:41 +0900

    Square 컴포넌트를 함수 컴포넌트로 변경

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,35 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button className="square" 
-      onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      squares:Array(9).fill(null)
-    }
+      squares: Array(9).fill(null),
+      xisNext: true
+    };
   }
 
-  handleClick(i){
+  handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares:squares});
+    squares[i] = "x";
+    this.setState({ squares: squares });
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-    onClick={()=>this.handleClick(i)} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
