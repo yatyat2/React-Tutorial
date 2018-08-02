55318258d076507e1d1f51a9f6fd44989e536465
yatyat2 <xowls2287@naver.com>
2018-08-02 18:55:34 +0900

    Square의 X,O 값 // Next player X,O 값 번갈아가며 출력하게 변경

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,14 +15,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xisNext: true
+      xIsNext: true
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "x";
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -35,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const status = "Next player: "+ (this.state.xIsNext? 'X':'O');
 
     return (
       <div>
