0906972db4602a3992e6e58abb48f11582dba8fa
yatyat2 <xowls2287@naver.com>
2018-08-02 16:19:44 +0900

    Square 클릭시 alert 실행

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={()=>alert('클릭')}>
           {this.props.value}
         </button>
       );
